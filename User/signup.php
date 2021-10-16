a42364dc9eeb258a47c119b77cdb4c720ea44515
Muhammad Mudasir <35227250+mudasirasfi@users.noreply.github.com>
2021-10-16 09:45:06 -0700

    Update signup.php

--- User/signup.php
+++ User/signup.php
@@ -10,26 +10,39 @@ $database = new Database();
 $db=$database->get_connection();
 $user = new User($db);
 
-//set user property values
-$user->name=$_POST['name'];
-$user->email=$_POST['email'];
-$user->password= base64_encode($_POST['password']);
-$user->reg_date= date('Y-m-d H:i:s');
+//set user property values with validations
+$pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";  
+if (empty($_POST["name"]) && (!preg_match("/^[a-zA-Z ]*$/",$_POST["name"])) ){  
+    $errMsg = "Error! You didn't enter the Name.";  
+             echo $errMsg;  
+} 
 
-//create the user
-if($user->signup()){
-    $user_arr=array(
-    "status" => http_response_code(),
-    "message" => "Successfully Signup",
-    "id name" => $user->name
-    );
+elseif (empty($_POST['email']) && !preg_match ($pattern,$_POST['email']) ){  
+    $ErrMsg = "Email is not valid.";  
+            echo $ErrMsg;  
 }
-else{
-    $user_arr = array(
+ else {  
+    $user->name=$_POST['name']; 
+    $user->email=$_POST['email'];  
+    $user->password= base64_encode($_POST['password']);
+    $user->reg_date= date('Y-m-d H:i:s');
+    //create the user
+        if($user->signup()){
+        $user_arr=array(
+        "status" => http_response_code(),
+        "message" => "Successfully Signup",
+        "id name" => $user->name
+        );
+    }
+        else{
+        $user_arr = array(
         "status" =>http_response_code(),
         "message" => $user->name." Already exists!"
-    );
-
+        );
     }
+    
 print_r(json_encode($user_arr));
+}  
+
+
 ?>
