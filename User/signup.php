382a1a6d045f28b784ff4c01ee5f7621adcbc5d9
Muhammad Mudasir <35227250+mudasirasfi@users.noreply.github.com>
2021-10-17 06:21:41 -0700

    Updates signup page by Mudasir

--- User/signup.php
+++ User/signup.php
@@ -4,45 +4,37 @@
 include_once '../config/database.php';
 
 //get user data
-include_once  '../User/user.php';
+include_once '../User/user.php';
 
 $database = new Database();
-$db=$database->get_connection();
+$db = $database->get_connection();
 $user = new User($db);
 
 //set user property values with validations
-$pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";  
-if (empty($_POST["name"]) && (!preg_match("/^[a-zA-Z ]*$/",$_POST["name"])) ){  
-    $errMsg = "Error! You didn't enter the Name.";  
-             echo $errMsg;  
-} 
-
-elseif (empty($_POST['email']) && !preg_match ($pattern,$_POST['email']) ){  
-    $ErrMsg = "Email is not valid.";  
-            echo $ErrMsg;  
-}
- else {  
-    $user->name=$_POST['name']; 
-    $user->email=$_POST['email'];  
-    $user->password= base64_encode($_POST['password']);
-    $user->reg_date= date('Y-m-d H:i:s');
+$pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";
+if (empty($_POST["name"]) && (!preg_match("/^[a-zA-Z ]*$/", $_POST["name"]))) {
+    $error_msg = "Error! You didn't enter the Name.";
+    echo $error_msg;
+} elseif (empty($_POST['email']) && !preg_match($pattern, $_POST['email'])) {
+    $error_msg = "Email is not valid.";
+    echo $error_msg;
+} else {
+    $user->name = $_POST['name'];
+    $user->email = $_POST['email'];
+    $user->password = base64_encode($_POST['password']);
+    $user->reg_date = date('Y-m-d H:i:s');
     //create the user
-        if($user->signup()){
-        $user_arr=array(
-        "status" => http_response_code(),
-        "message" => "Successfully Signup",
-        "id name" => $user->name
-        );
-    }
-        else{
+    if ($user->signup()) {
         $user_arr = array(
-        "status" =>http_response_code(),
-        "message" => $user->email." Already exists!"
+            "status" => http_response_code(),
+            "message" => "Successfully Signup",
+            "id name" => $user->name,
+        );
+    } else {
+        $user_arr = array(
+            "status" => http_response_code(),
+            "message" => $user->email . " Already exists!",
         );
     }
-    
-print_r(json_encode($user_arr));
-}  
-
-
-?>
+    print_r(json_encode($user_arr));
+}
