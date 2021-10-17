8ff8bb79f6debdcaf7595ac421e8c4ab9bbea826
Nasik Naveed <53467503+naveednasik@users.noreply.github.com>
2021-10-17 17:27:29 +0500

    Add files via upload

---
+++ search.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="db.php">
+	<div align="center" style="margin-top: 65px;">
+		<input type="text" name="name" placeholder="Enter the name">
+		<button name="search" type="submit">Search</button>
+	</div>
+
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
