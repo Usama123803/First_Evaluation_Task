3e761bcfbe9c6d002e05dff10bde0a7494a1c2c9
Muhammad Mudasir <35227250+mudasirasfi@users.noreply.github.com>
2021-10-17 06:11:20 -0700

    Update login.php

--- User/login.php
+++ User/login.php
@@ -2,40 +2,37 @@
 
 //get database connection and object files
 include_once '../config/database.php';
-include_once  '../User/user.php';
+include_once '../User/user.php';
 
 // get database connection
 $database = new Database();
-$db=$database->get_connection();
+$db = $database->get_connection();
 
 // prepare user object
 $user = new User($db);
 
 // set ID property of user to be edited
-$user->username = isset($_GET['username']) ? $_GET['username'] : die();
+$user->username = isset($_GET['name']) ? $_GET['name'] : die();
 $user->password = base64_encode(isset($_GET['password']) ? $_GET['password'] : die());
 
 // read the details of user to be edited
 $stmt = $user->login();
-if($stmt->rowCount() > 0){
+if ($stmt->rowCount() > 0) {
     // get retrieved row
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     // create array
     $user_arr = array(
-        "status"   => true,
-        "message"  => "Successfully login",
-        "id"       => $row['id'],
-        "username" => $row['username']
+        "status" => true,
+        "message" => "Successfully login",
+        "id" => $row['id'],
+        "name" => $row['name'],
     );
-}
-else{
+} else {
     $user_arr = array(
-        "status"  => false,
-        "message" => "Invalid Username or Password!"
+        "status" => false,
+        "message" => "Invalid Username or Password!",
     );
 }
 
 // make it json format
 print_r(json_encode($user_arr));
-
-?>
